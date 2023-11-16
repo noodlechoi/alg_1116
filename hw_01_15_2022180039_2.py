d0bef62fb2e2a101a82175a6747954a3a622286e
noodlechoi <choime321@gmail.com>
2023-11-16 19:59:49 +0900

    error modified

--- hw_01_15_2022180039_2.py
+++ hw_01_15_2022180039_2.py
@@ -3,16 +3,16 @@ d = [2,5,9,3,7,2,3,8,2,3]
 mc = len(d) - 1
 
 # 10 X 10개의 0을 넣은 이차원 배열
-C = [[0 for _ in range(mc + 1) for _ in range(mc + 1)]]
+C = [[0 for _ in range(mc + 1)] for _ in range(mc + 1)]
 # 마지막 곱셈의 위치?
-P = [[0 for _ in range(mc + 1) for _ in range(mc + 1)]]
+P = [[0 for _ in range(mc + 1)] for _ in range(mc + 1)]
 
 INF = float('INF')
 
 # 부분 문제 크기 : 1일 때는 0이기 때문에 2부터 루프 돌기
 for sps in range(2, mc + 1): # Sub-Problem Size
     # 부분 문제의 개수
-    spc = mc - sps + 1# Sub-Problem Count
+    spc = mc - sps + 1 # Sub-Problem Count
     for beg in range(1, spc + 1):
         end = beg + sps - 1 # end is inclusive
         C[beg][end] = INF
